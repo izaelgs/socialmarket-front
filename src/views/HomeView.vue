c36e09dc21a33ad1760f97afce685f9664db2835
izaelgs <izael-123@outlook.com>
2024-02-03 18:05:10 -0300

    Added footer with contact columns to Vue component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -33,6 +33,7 @@
           "
         ></div>
       </div>
+
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="hidden sm:mb-8 sm:flex sm:justify-center" v-motion-fade>
           <div
@@ -83,36 +84,85 @@
           </div>
         </div>
       </div>
-      <div
-        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
-        <div
-          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style="
-            clip-path: polygon(
-              74.1% 44.1%,
-              100% 61.6%,
-              97.5% 26.9%,
-              85.5% 0.1%,
-              80.7% 2%,
-              72.5% 32.5%,
-              60.2% 62.4%,
-              52.4% 68.1%,
-              47.5% 58.3%,
-              45.2% 34.5%,
-              27.5% 76.7%,
-              0.1% 64.9%,
-              17.9% 100%,
-              27.6% 76.8%,
-              76.1% 97.7%,
-              74.1% 44.1%
-            );
-          "
-        ></div>
-      </div>
     </div>
   </div>
+
+  <div
+    class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+    aria-hidden="true"
+  >
+    <div
+      class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+      style="
+        clip-path: polygon(
+          74.1% 44.1%,
+          100% 61.6%,
+          97.5% 26.9%,
+          85.5% 0.1%,
+          80.7% 2%,
+          72.5% 32.5%,
+          60.2% 62.4%,
+          52.4% 68.1%,
+          47.5% 58.3%,
+          45.2% 34.5%,
+          27.5% 76.7%,
+          0.1% 64.9%,
+          17.9% 100%,
+          27.6% 76.8%,
+          76.1% 97.7%,
+          74.1% 44.1%
+        );
+      "
+    ></div>
+  </div>
+
+  <footer class="text-white">
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+        <!-- Column 1: Contact Information -->
+        <div class="grid grid-cols-2 gap-8 xl:col-span-1">
+          <div>
+            <h2 class="text-lg font-medium tracking-wide">Contact</h2>
+            <ul class="mt-4 space-y-4">
+              <!-- <li>
+                <p class="text-base">Address: Main St, 123</p>
+              </li> -->
+              <li>
+                <p class="text-base">Phone: +55 (27) 9 8812-6640</p>
+              </li>
+              <li>
+                <p class="text-base">Email: izael.dev@gmail.com</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- Column 2: Social Media Links -->
+        <div class="mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-2">
+          <div>
+            <h2 class="text-lg font-medium tracking-wide">Social Media</h2>
+            <ul class="mt-4 space-y-4">
+              <li>
+                <a href="https://twitter.com/Izael65" class="text-base hover:text-gray-300"
+                  >Twitter</a
+                >
+              </li>
+              <li>
+                <a href="https://www.facebook.com/izael.gs" class="text-base hover:text-gray-300"
+                  >Facebook</a
+                >
+              </li>
+              <li>
+                <a href="https://www.instagram.com/izael_hs/" class="text-base hover:text-gray-300"
+                  >Instagram</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup>
