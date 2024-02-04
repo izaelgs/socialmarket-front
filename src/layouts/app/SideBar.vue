58548841a64827f77c8a9bc36a5c1bb9917d7a9e
izaelgs <izael-123@outlook.com>
2024-02-04 17:20:50 -0300

    add logout functionality

--- src/layouts/app/SideBar.vue
+++ src/layouts/app/SideBar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="
     fixed top-0 left-0 h-screen w-16 m-0 flex flex-col
+    justify-between items-center
     bg-gray-100 dark:bg-gray-900
     text-gray-900 dark:text-white
     shadow
@@ -8,6 +9,8 @@
     <ul>
       <SideBarIcon path="./" icon="material-symbols:house-rounded" tooltip="Home" />
       <SideBarIcon path="edit-profile" icon="material-symbols:manage-accounts-rounded" tooltip="Edit Profile" />
+    </ul>
+    <div>
       <div class="sidebar-icon group cursor-pointer" @click="toggleDarkMode">
         <div class="dark:text-gray-100 text-gray-900">
           <Icon v-if="darkMode" icon="material-symbols:light-mode" />
@@ -15,7 +18,13 @@
         </div>
         <span class="sidebar-tooltip group-hover:scale-100">{{ darkMode ? 'Ligth Mode' : 'Dark Mode'}}</span>
       </div>
-    </ul>
+      <div class="sidebar-icon group cursor-pointer" @click="handleLogout">
+        <div class="dark:text-gray-100 text-gray-900">
+          <Icon icon="material-symbols:logout" />
+        </div>
+        <span class="sidebar-tooltip group-hover:scale-100">Logout</span>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -24,10 +33,23 @@ import SideBarIcon from '@/layouts/app/SideBarIcon.vue';
 import useThemeStorage from '@/stores/ThemeStorage';
 import { computed } from 'vue';
 import { Icon } from '@iconify/vue';
+import { useAxiosStore } from '@/services/axiosStore';
+import { useUserStore } from '@/services/userStore';
+import { useRouter } from 'vue-router';
+
+const axios = useAxiosStore()
+const router = useRouter()
+const { clearUser } = useUserStore()
 
 const { toggleDarkMode } = useThemeStorage();
 const darkMode = computed({
   get() { return useThemeStorage().darkMode },
   set(val) { useThemeStorage().darkMode = val }
 })
+
+const handleLogout = async () => {
+  await axios.get('/auth/logout')
+  clearUser()
+  router.push('/')
+}
 </script>
