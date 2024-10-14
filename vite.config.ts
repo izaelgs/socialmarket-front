0fe2bd1bbbe3dfe2c02216772239337064843fe0
izaelgs <izael-123@outlook.com>
2024-10-14 11:16:26 -0300

    rebuild vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default ({ mode }: {mode: string}) => {
@@ -24,7 +25,7 @@ export default ({ mode }: {mode: string}) => {
     ],
     resolve: {
       alias: {
-        '@/': `${path.resolve(__dirname, 'src')}/`
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
   })
